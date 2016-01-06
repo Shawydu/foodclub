d1c925a7796b46742f61ca43932934960101e783
Shawydu <dxydyl2@gmail.com>
2016-01-06 15:54:17 +0000

    add model places

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105171329) do
+ActiveRecord::Schema.define(version: 20160105210140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "palces", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "address"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "places", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "address"
