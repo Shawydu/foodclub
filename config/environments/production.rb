32bbaad55afc76712bc8e8c45141ae67df9fe748
Shawydu <dxydyl2@gmail.com>
2016-01-13 22:54:54 +0000

    Addon Mailgun to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+
+  # Use mail service through MailGun
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
